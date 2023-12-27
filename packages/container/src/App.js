40ad12ce2188b2ffa844477a31c01d663e1883ea
Radovan Tkac <radovan.tkac@zentity.com>
2023-12-27 17:28:57 +0100

    feat: updated container app text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Container app is up and running...</h1>
+      <h1>Container is up and running...</h1>
       <hr />
       <MarketingApp />
     </div>
